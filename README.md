01f4a48eae79cd50f22f94d1b4a5a457b724c14b
M7ilan <distny2004@gmail.com>
2023-10-10 13:54:59 +0300

    fix https and host

--- README.md
+++
@@ -1,5 +0,0 @@
-# Getting Started
-
-1. Clone the repository with this command: `git clone https://github.com/OmarAfet/NextJS-Template.git .`
-2. install the packages with this command: `npm i`
-3. Run `npm run dev` and enjoy!.
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,10 +2,11 @@ import NextAuth from "next-auth";
 import BungieProvider from "next-auth/providers/bungie";
 
 const handler = NextAuth({
+	secret: process.env.NEXTAUTH_SECRET,
 	providers: [
 		BungieProvider({
 			clientId: process.env.BUNGIE_CLIENT_ID,
-			clientSecret: process.env.BUNGIE_SECRET,
+			clientSecret: process.env.BUNGIE_CLIENT_SECRET,
 			headers: {
 				"X-API-Key": process.env.BUNGIE_API_KEY,
 			},
