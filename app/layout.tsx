cc98aa3736a8501af8a7d8ca6b97a70f65551c7e
M7ilan <distny2004@gmail.com>
2023-10-16 18:05:48 +0300

    add the provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,28 @@ import "./globals.css";
 import Navbar from "@Components/Navbar";
 import Footer from "@Components/Footer";
 import Providers from "./Provider";
+import { getServerSession } from "next-auth";
 import type { Metadata } from "next";
 import { Toaster } from "react-hot-toast";
-
+import SessionProvider from "@Components/SessionProvider";
 export const metadata: Metadata = {
 	title: "Next Auth Bungie Provider",
 	description: "Next Auth Bungie Provider Example App",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+	const session = await getServerSession();
+
 	return (
 		<html>
 			<body className="bg-white text-OpenColor-gray-7 dark:bg-OpenColor-gray-9 dark:text-OpenColor-gray-1">
 				<Toaster position="top-center" reverseOrder={false} />
 				<Providers>
-					<Navbar />
-					<main>{children}</main>
-					<Footer />
+					<SessionProvider session={session}>
+						<Navbar />
+						<main>{children}</main>
+						<Footer />
+					</SessionProvider>
 				</Providers>
 			</body>
 		</html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+"use client";
+
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+	const { data: session } = useSession();
+	if (session) {
+		return (
+			<>
+				<div>Logged in Successfully</div>
+			</>
+		);
+	}
 	return (
 		<>
 			<div className="center flex flex-col gap-4">
-				<Link href="/api/auth/callback/bungie" className="title btn-0">Authinticate with Bungie</Link>
+				<Link href="/api/auth/callback/bungie" className="title btn-0">
+					Authinticate with Bungie
+				</Link>
 			</div>
 		</>
 	);
